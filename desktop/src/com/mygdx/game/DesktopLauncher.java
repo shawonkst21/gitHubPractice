8744668b8179be9fd9767912c39df10045eef920
shawon <sadmanshawonkst11@gmail.com>
2024-07-03 10:09:35 +0600

    change

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("My First Java Game Project");
+		config.setTitle("space shooter");
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
